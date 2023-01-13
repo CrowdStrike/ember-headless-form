0d53e2c2c762c9f833ea87f62d1d9a6933f26cb1
Simon Ihmig <simon.ihmig@gmail.com>
2023-01-13 18:27:03 +0100

    field.label contextual component

--- ember-headless-form/src/components/-private/field.ts
+++ ember-headless-form/src/components/-private/field.ts
@@ -1,5 +1,7 @@
 import Component from '@glimmer/component';
 import { HeadlessFormData } from '../headless-form';
+import LabelComponent from './field/label';
+import { WithBoundArgs } from '@glint/template';
 
 export interface HeadlessFormFieldComponentSignature<
   DATA extends HeadlessFormData
@@ -9,13 +11,19 @@ export interface HeadlessFormFieldComponentSignature<
     name: keyof DATA;
   };
   Blocks: {
-    default: [];
+    default: [
+      {
+        label: WithBoundArgs<typeof LabelComponent, 'fieldId'>;
+      }
+    ];
   };
 }
 
-export default class HeadlessFormComponent<
+export default class HeadlessFormFieldComponent<
   DATA extends HeadlessFormData
 > extends Component<HeadlessFormFieldComponentSignature<DATA>> {
+  LabelComponent = LabelComponent;
+
   get value(): DATA[keyof DATA] | undefined {
     return this.args.data[this.args.name];
   }
--- test-app/tests/integration/components/headless-form-test.ts
+++ test-app/tests/integration/components/headless-form-test.ts
@@ -11,28 +11,75 @@ module('Integration | Component | headless-form', function (hooks) {
     await render(hbs`<HeadlessForm class="foo" novalidate />`);
 
     assert.dom('form').exists('it renders as <form>');
-    assert.dom('form').hasClass('foo').hasAttribute('novalidate');
+    assert
+      .dom('form')
+      .hasClass('foo', 'it accepts custom HTML classes')
+      .hasAttribute('novalidate', '', 'it accepts arbitrary HTML attributes');
   });
 
-  test('it yields field component', async function (this: TestContext & {
-    data: { firstName?: string };
-  }, assert) {
-    this.data = { firstName: 'Simon' };
+  module('form.field', function () {
+    test('from yields field component', async function (this: TestContext & {
+      data: { firstName?: string };
+    }, assert) {
+      this.data = { firstName: 'Simon' };
 
-    await render<typeof this>(hbs`
-      <HeadlessForm @data={{this.data}} as |form|>
-        <form.field @name="firstName">
-          <div data-test-user-content>foo</div>
-        </form.field>
-      </HeadlessForm>
-    `);
+      await render<typeof this>(hbs`
+        <HeadlessForm @data={{this.data}} as |form|>
+          <form.field @name="firstName">
+            <div data-test-user-content>foo</div>
+          </form.field>
+        </HeadlessForm>
+      `);
 
-    assert
-      .dom('[data-test-user-content]')
-      .exists('form field can render user content');
+      assert
+        .dom('[data-test-user-content]')
+        .exists('form field can render user content');
 
-    assert
-      .dom('form > [data-test-user-content]')
-      .exists('does not render anything on its own');
+      assert
+        .dom('form > [data-test-user-content]')
+        .exists('does not render anything on its own');
+    });
+
+    test('Glint: @name argument only expects keys of @data', async function (this: TestContext & {
+      data: { firstName?: string };
+    }, assert) {
+      assert.expect(0);
+      this.data = { firstName: 'Simon' };
+
+      await render<typeof this>(hbs`
+        <HeadlessForm @data={{this.data}} as |form|>
+          {{! @glint-expect-error this is expected to error when running glint checks! }}
+          <form.field @name="lastName">
+            <div data-test-user-content>foo</div>
+          </form.field>
+        </HeadlessForm>
+      `);
+    });
+  });
+
+  module('field.label', function () {
+    test('field yields label component', async function (this: TestContext & {
+      data: { firstName?: string };
+    }, assert) {
+      this.data = { firstName: 'Simon' };
+
+      await render<typeof this>(hbs`
+        <HeadlessForm @data={{this.data}} as |form|>
+          <form.field @name="firstName" as |field|>
+            <field.label class="my-label" data-test-label>First Name</field.label>
+          </form.field>
+        </HeadlessForm>
+      `);
+
+      assert
+        .dom('label')
+        .hasText('First Name', 'it renders block content')
+        .hasClass('my-label', 'it accepts custom HTML classes')
+        .hasAttribute(
+          'data-test-label',
+          '',
+          'it accepts arbitrary HTML attributes'
+        );
+    });
   });
 });
