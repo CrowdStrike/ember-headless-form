c523a90c3ff0a02f6e969d91f91c135f14c9d661
Simon Ihmig <simon.ihmig@gmail.com>
2023-01-16 10:31:28 +0100

    Use native unique-id helper

--- ember-headless-form/src/components/-private/field.ts
+++ ember-headless-form/src/components/-private/field.ts
@@ -30,16 +30,4 @@ export default class HeadlessFormFieldComponent<
   get value(): DATA[keyof DATA] | undefined {
     return this.args.data[this.args.name];
   }
-
-  // copy-pasted from https://github.com/emberjs/ember.js/blob/master/packages/@ember/-internals/glimmer/lib/helpers/unique-id.ts
-  // we can remove this once we only support Ember 4.4+!
-  // we could have also used https://github.com/ctjhoa/ember-unique-id-helper-polyfill, but this is a v1 addon and we better keep our addon light-weight
-  uniqueId() {
-    // @ts-expect-error this one-liner abuses weird JavaScript semantics that
-    // TypeScript (legitimately) doesn't like, but they're nonetheless valid and
-    // specced.
-    return ([3e7] + -1e3 + -4e3 + -2e3 + -1e11).replace(/[0-3]/g, (a) =>
-      ((a * 4) ^ ((Math.random() * 16) >> (a & 2))).toString(16)
-    );
-  }
 }
