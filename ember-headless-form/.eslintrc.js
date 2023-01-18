e77cba1bb74ad055bd94ff274870ea64ab5ca663
Simon Ihmig <simon.ihmig@gmail.com>
2023-01-18 17:58:12 +0100

    Add comment to VSCode prettier workaround

--- ember-headless-form/.eslintrc.js
+++ ember-headless-form/.eslintrc.js
@@ -1,50 +1,6 @@
 'use strict';
 
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  plugins: ['ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    browser: true,
-  },
-  rules: {},
-  overrides: [
-    // ts files
-    {
-      files: ['**/*.ts'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      rules: {
-        // Add any custom rules here
-      },
-    },
-    // node files
-    {
-      files: [
-        './.eslintrc.js',
-        './.prettierrc.js',
-        './.template-lintrc.js',
-        './addon-main.js',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-      env: {
-        browser: false,
-        node: true,
-      },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-    },
-  ],
-};
+const { configs } = require('@nullvoxpopuli/eslint-configs');
+
+// accommodates: JS, TS, App, and Addon
+module.exports = configs.ember();
