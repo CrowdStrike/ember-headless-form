bf614b11b17aaa3e376e9e2f1bb494ce5f3ea5c4
Simon Ihmig <simon.ihmig@gmail.com>
2023-03-16 12:17:35 +0100

    Convert addon to use template tag

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,20 @@ module.exports = {
   plugins: ['prettier-plugin-ember-template-tag'],
   singleQuote: true,
   templateSingleQuote: false,
+  // this was required to make the VSCode + Prettier work correctly with <template>, see https://github.com/gitKrystan/prettier-plugin-ember-template-tag/issues/38
+  // we should roll this back once that issue has been fixed!
+  overrides: [
+    {
+      files: '*.gjs',
+      options: {
+        parser: 'ember-template-tag',
+      },
+    },
+    {
+      files: '*.gts',
+      options: {
+        parser: 'ember-template-tag',
+      },
+    },
+  ],
 };
