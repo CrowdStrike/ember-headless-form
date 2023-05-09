590d6e127f67ade79656252bf04e77ffea1a59ac
Kelvin Luck <git@kelvinluck.com>
2023-05-09 23:10:22 +0100

    feat: `_syncPnpm` operates in "watch" mode
    So that you can run it once and it will automatically copy the
    `dist` folder over whenever the contents of the folder changes.
    
    I included a bit of debouncing so that we only sync the contents
    once per set of changes

--- internal/sync-pnpm/index.js
+++ internal/sync-pnpm/index.js
@@ -8,11 +8,14 @@ import { hardLinkDir } from '@pnpm/fs.hard-link-dir';
 import resolvePackagePath from 'resolve-package-path';
 import Debug from 'debug';
 import lockfile from 'proper-lockfile';
+import Watcher from 'watcher';
 
 const require = createRequire(import.meta.url);
 const debug = Debug('sync-pnpm');
 
 const syncDir = './dist';
+
+const DEBOUNCE_INTERVAL = 50;
 
 export default async function syncPnpm(dir = process.cwd()) {
   const root = await findRoot(dir);
@@ -30,33 +33,72 @@ export default async function syncPnpm(dir = process.cwd()) {
       ownDependencies.includes(p.packageJson.name)
   );
 
+  let paths = {};
+
   for (const pkg of packagesToSync) {
     const syncFrom = join(pkg.dir, syncDir);
+
     const resolvedPackagePath = dirname(
       resolvePackagePath(pkg.packageJson.name, dir)
     );
     const syncTo = join(resolvedPackagePath, syncDir);
 
     if (await pathExists(syncFrom)) {
-      let releaseLock;
-      try {
-        releaseLock = await lockfile.lock(syncTo, { realpath: false });
-        debug(`lockfile created for syncing to ${syncTo}`);
-      } catch (e) {
-        debug(
-          `lockfile already exists for syncing to ${syncTo}, some other sync process is already handling this directory, so skipping...`
-        );
-        continue;
-      }
-
-      if (await pathExists(syncTo)) {
-        await remove(syncTo);
-        debug(`removed ${syncTo} before syncing`);
-      }
-
-      debug(`syncing from ${syncFrom} to ${syncTo}`);
-      await hardLinkDir(syncFrom, [syncTo]);
-      releaseLock();
+      paths[syncFrom] = syncTo;
     }
   }
+
+  let fromPaths = Object.keys(paths);
+  let watcher = new Watcher(fromPaths);
+
+  let dirtyPaths = [];
+
+  watcher.on('all', (event, targetPath, targetPathNext) => {
+    dirtyPaths.push(targetPath);
+  });
+
+  async function handleDirtyPaths() {
+    if (dirtyPaths.length) {
+      let foundFromPaths = {};
+      for (let dirtyPath of dirtyPaths) {
+        let path = fromPaths.find((p) => dirtyPath.startsWith(p));
+        if (path === undefined) {
+          debug(`path not under watched root ${dirtyPath}`);
+        } else {
+          foundFromPaths[path] = true;
+        }
+      }
+      dirtyPaths = [];
+
+      for (let foundFromPath of Object.keys(foundFromPaths)) {
+        await syncDependency(foundFromPath, paths[foundFromPath]);
+      }
+    }
+    setTimeout(handleDirtyPaths, DEBOUNCE_INTERVAL);
+  }
+
+  handleDirtyPaths();
+}
+
+async function syncDependency(syncFrom, syncTo) {
+
+  let releaseLock;
+  try {
+    releaseLock = await lockfile.lock(syncTo, { realpath: false });
+    debug(`lockfile created for syncing to ${syncTo}`);
+  } catch (e) {
+    debug(
+      `lockfile already exists for syncing to ${syncTo}, some other sync process is already handling this directory, so skipping...`
+    );
+    return;
+  }
+
+  if (await pathExists(syncTo)) {
+    await remove(syncTo);
+    debug(`removed ${syncTo} before syncing`);
+  }
+
+  debug(`syncing from ${syncFrom} to ${syncTo}`);
+  await hardLinkDir(syncFrom, [syncTo]);
+  releaseLock();
 }
